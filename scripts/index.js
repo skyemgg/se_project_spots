a8f76c65cd7dd00bc7cc07175af593d9d859ee84
Skye Grady <skyegrady16@gmail.com>
2025-08-27 12:35:29 -0500

    spots modal update

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 const editProfileButtion = document.querySelector(".profile__edit");
-const editProfile = document.querySelector(".edit-profile-modal");
+const editProfile = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfile.querySelector(".modal__close-button");
 
 const newPostButtion = document.querySelector(".profile__post");
-const newPost = document.querySelector(".new-post-modal");
+const newPost = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPost.querySelector(".modal__close-button");
 
 editProfileButtion.addEventListener("click", function () {
-  editProfile.classList.add("modal_is-open");
+  editProfile.classList.add("modal_is-opened");
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfile.classList.remove("modal_is-open");
+  editProfile.classList.remove("modal_is-opened");
 });
 
 newPostButtion.addEventListener("click", function () {
-  newPost.classList.add("modal_is-open");
+  newPost.classList.add("modal_is-opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newpost.classList.remove("modal_is-open");
+  newpost.classList.remove("modal_is-opened");
 });
